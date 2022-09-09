e64c31aebec0f0e25defadc0e805423874fcfc37
imcalled <queenieyear@gmail.com>
2022-09-09 12:53:36 +0100

    updated webdriver location in environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 def before_all(context):
-    context.browser = webdriver.Chrome(r"C:\Users\Admin\Desktop\lbg\webdrivers\chromedriver.exe")
+    context.browser = webdriver.Chrome(r"C:\Users\Admin\Documents\LBG-Python-API\webdrivers\chromedriver.exe")
     context.browser.maximize_window()
 
 def after_all(context):
