d1e18d1f2a667cebae0338d0a53fdd4eeb2dd523
PCMBarber <57707405+PCMBarber@users.noreply.github.com>
2023-02-22 08:27:19 +0000

    Delete docker.sh

--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash/
-docker build -t imcalled/lbg-api:latest . 
-docker push imcalled/lbg-api:latest
-
-docker stop lbg-container
-docker rm lbg-container
-docker run -d -p 8080:8080 --name lbg-container imcalled/lbg-api:latest
