25cd177c461484c21e26f7eb6cbe5db6243196ef
Alexander Yirenkyi <yirenkyialexander@gmail.com>
2024-06-14 11:07:19 +0100

    corrected getenv

--- lbg.py
+++ lbg.py
@@ -206,7 +206,7 @@ def delete_one(_id):
 # module import protection
 if __name__ == '__main__':
     # get app to serve
-    PORT = (os.gentenv('PORT', 8080))
+    PORT = (os.getenv('PORT', 8080))
     HOST = '0.0.0.0'
     print(f'API Listening on http://{HOST}:{PORT}')
     app.run(host=HOST, port=PORT, debug=True)
