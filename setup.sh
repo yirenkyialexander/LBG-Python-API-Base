d22a194045d69a9cd66a9242d4bb27ced0a57f62
Alexander Yirenkyi <yirenkyialexander@gmail.com>
2024-06-14 12:00:26 +0100

    update port to 80 in setup.sh

--- setup.sh
+++ setup.sh
@@ -28,7 +28,7 @@ modify_app() {
 run_docker() {
     echo "Running Docker container..."
     sleep 3
-    docker run -d -p 8080:$PORT -e PORT=$PORT $DOCKER_IMAGE
+    docker run -d -p 80:$PORT -e PORT=$PORT $DOCKER_IMAGE
 }
 # Main script execution
 echo "Starting build process..."
